9c281ddcd90910858de4f2fcdf6445409ceabc10
NussN <NussN@wsdot.wa.gov>
2019-12-17 09:01:09 -0800

    Add project files.

---
+++ wwwroot/js/components/helloworld.vue
@@ -0,0 +1,13 @@
+﻿<template>
+    <h2>{{ message }}</h2>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message : 'Hello Vue Component!'
+            }
+        }
+    }
+</script>
